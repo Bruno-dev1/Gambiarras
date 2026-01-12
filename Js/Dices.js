3b97965fa116c02ce7a21ef4085ffd312857520f
Bruno Santiago <brunonasifalso@gmail.com>
2026-01-12 13:00:52 -0300

    ´´

---
+++ Js/Dices.js
@@ -0,0 +1,34 @@
+const prompt = require("prompt-sync")();
+console.log("Choose your dice");
+console.log("d20");
+console.log("d12");
+console.log("d8");
+console.log("d6");
+console.log("d4");
+
+const dice = prompt("What dice do you want to roll?");
+
+switch (dice) {
+    case "d20":
+        console.log("Rolling d20");
+        console.log("It's a " + (Math.floor(Math.random() * 20) + 1));
+        break;
+    case "d12":
+        console.log("Rolling d12");
+        console.log("It's a " + (Math.floor(Math.random() * 12) + 1));
+        break;
+    case "d8":
+        console.log("Rolling d8");
+        console.log("It's a " + (Math.floor(Math.random() * 8) + 1));
+        break;
+    case "d6":
+        console.log("Rolling d6");
+        console.log("It's a " + (Math.floor(Math.random() * 6) + 1));
+        break;
+    case "d4":
+        console.log("Rolling d4");
+        console.log("It's a " + (Math.floor(Math.random() * 4) + 1));
+        break;
+    default:
+        console.log("Invalid dice");
+}
