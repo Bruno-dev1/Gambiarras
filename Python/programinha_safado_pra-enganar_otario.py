7a67819e25cf20e9b2fb056c4c7feb8a1850c52f
Bruno Santiago <brunonasifalso@gmail.com>
2026-02-16 14:06:03 -0300

    6

--- Python/programinha_safado_pra-enganar_otario.py
+++
@@ -1,9 +0,0 @@
-import pyautogui as auto
-import
-auto.PAUSE = 2
-
-for i in range(1, 100):
-    if i % 2 == 0:
-        auto.write(f"if({i}%2==0){{console.log('o numero é par')}}")
-    else:
-        auto.write(f"if({i}%2==0){{console.log('o numero é impar')}}")
